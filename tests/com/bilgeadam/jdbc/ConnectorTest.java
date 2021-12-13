bb040a2f46ca02b91dbc5230702d6b75b88a0efa
Muhammet KILIÇ <muhammetkilic@yahoo.com>
2021-12-13 17:55:55 +0300

    COMPLETED!!!

--- tests/com/bilgeadam/jdbc/ConnectorTest.java
+++ tests/com/bilgeadam/jdbc/ConnectorTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class ConnectorTest {
 
     @BeforeEach
--- tests/com/bilgeadam/jdbc/UserControllerTest.java
+++ tests/com/bilgeadam/jdbc/UserControllerTest.java
@@ -1,10 +1,8 @@
 package com.bilgeadam.jdbc;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
     private UserController controller;
