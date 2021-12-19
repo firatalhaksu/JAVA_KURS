f86f18977eb41752af028104292943f975d56b5f
Muhammet KILIÇ <muhammetkilic@yahoo.com>
2021-12-19 11:31:16 +0300

    COMPLETED!!!

--- src/com/bilgeadam/jdbc/UserController.java
+++ src/com/bilgeadam/jdbc/UserController.java
@@ -2,7 +2,9 @@ package com.bilgeadam.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Set;
 
 public class UserController {
     public void addUser(Long ID, String name){
@@ -14,11 +16,29 @@ public class UserController {
             statement.setString(2, name.substring(0, name.lastIndexOf("\s")));
             statement.setString(3, name.substring(name.lastIndexOf("\s")));
 
-            statement.execute();
-
+            statement.executeUpdate();
 
         } catch (SQLException e) {
             System.out.println("There is a problem with Database!");
         }
+    }
+
+    public Set<User> getUser(){
+        String sql = "SELECT * FROM public.\"Users\"";
+
+        try(Connection conn = Connector.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            ResultSet result = statement.executeQuery();
+            return convertToUsers(result);
+
+        } catch (SQLException e) {
+            System.out.println("There is a problem with Database!");
+            return null;
+        }
+    }
+
+    private Set<User> convertToUsers(ResultSet result){
+        return null;
     }
 }
