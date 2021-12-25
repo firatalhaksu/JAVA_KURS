7b7ac531baa8901c66f1f87dcde245a90218c7e3
Muhammet KILIÇ <muhammetkilic@yahoo.com>
2021-12-25 11:57:47 +0300

    Convert to Maven

--- src/main/java/com/bilgeadam/hibernate/Customer.java
+++ src/main/java/com/bilgeadam/hibernate/Customer.java
@@ -1,14 +1,6 @@
 package com.bilgeadam.hibernate;
 
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.Table;
-
 import java.util.UUID;
-
-@DynamicUpdate
-@DynamicInsert
-@Table(appliesTo = "customers")
 
 public class Customer {
 
